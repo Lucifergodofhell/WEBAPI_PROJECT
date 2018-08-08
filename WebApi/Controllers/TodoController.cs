07900ae737e96853826343996de57f9430b064f9
Ravi Kumar Dhal <RDhal@logisticsdev.intra>
2018-08-08 14:01:56 +0545

    Adding the update function in the TodoController.cs

--- WebApi/Controllers/TodoController.cs
+++ WebApi/Controllers/TodoController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using WebApI.Models;
 
+
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebApI.Controllers
@@ -15,16 +16,19 @@ namespace WebApI.Controllers
 	[ApiController]
 	public class TodoController : Controller
 	{
-		public string path= @"C:\Users\rdhal\source\repos\WebApI\WebApI\Data\Data.json";
+		public string path= @"C:\Users\rdhal\source\repos\WebApI\WEBAPI_PROJECT\WebApI\Data\Data.json";
 		[HttpPost]
 		public void Create(TodoItem item)
 		{
-			List<TodoItem> todo = new List<TodoItem>();
-			todo.Add(item);
-			string json = JsonConvert.SerializeObject(todo.ToArray());
-			System.IO.File.AppendAllText(path, json+ Environment.NewLine);
+			var jsonData = System.IO.File.ReadAllText(path);
+			// De-serialize to object or create new list
+			var List = JsonConvert.DeserializeObject<List<TodoItem>>(jsonData)
+					  ?? new List<TodoItem>();
+			List.Add(item);
+			jsonData = JsonConvert.SerializeObject(List);
+			System.IO.File.WriteAllText(path, jsonData+Environment.NewLine);
 
-		}
+		}	
 		[HttpGet]
 		public string GetAll()
 		{	
@@ -35,8 +39,25 @@ namespace WebApI.Controllers
 		[HttpGet("{id}")]
 		public string GetbyId(long id)
 		{
-			string user="term";
+			string user="";
 			return user;
+		}
+		[HttpPut("{id}")]
+		public void Update(long id, TodoItem item)
+		{
+			var jsonData = System.IO.File.ReadAllText(path);
+			var list = JsonConvert.DeserializeObject<List<TodoItem>>(jsonData)
+				  ?? new List<TodoItem>();
+
+			var temp=list.Find(term => term.Id==id);
+			if (temp != null)
+			{
+				temp.Name = item.Name;
+				temp.Pass = item.Pass;
+				temp.Email = item.Email;
+				jsonData = JsonConvert.SerializeObject(list);
+				System.IO.File.WriteAllText(path, jsonData);
+			}
 		}
 	}
 }
